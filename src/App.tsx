a6a2e48b017d9f2ac8d180a9a9a08b01833a87e0
yoidea <yoidea.cc@gmail.com>
2022-12-31 13:40:35 +0900

    add initial files

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,76 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [isRinging, setRinging] = useState(false);
+  const [hasPermission, setPermission] = useState(false);
+  const bell = new Audio("/bell.wav");
+  const effectTime = 3000;
+
+  const ring = () => {
+    setRinging(true);
+    setCount(count => count + 1);
+    bell.pause();
+    bell.currentTime = 0;
+    bell.play();
+    setTimeout(() => {
+      setRinging(false);
+    }, effectTime);
+  };
+
+  const handlePermissionRequest = () => {
+    // 加速度センサーの使用許可
+    DeviceOrientationEvent.requestPermission()
+      .then(function(response) {
+        if (response === "granted") {
+          window.addEventListener("devicemotion", devicemotionHandler);
+          setPermission(true);
+        }
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  };
+
+  useEffect(() => {
+    if (window.DeviceOrientationEvent) {
+      if (
+        DeviceOrientationEvent.requestPermission &&
+        typeof DeviceOrientationEvent.requestPermission === "function"
+      ) {
+      } else {
+        setPermission(true);
+        window.addEventListener("devicemotion", e => {
+          if (e.acceleration.x > 5) {
+            ring();
+          }
+        });
+      }
+    }
+  }, []);
 
   return (
     <div className="App">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a onClick={ring}>
+          <img
+            src="/bell.png"
+            className={isRinging ? "bell ring" : "bell"}
+            alt="除夜の鐘"
+          />
         </a>
       </div>
-      <h1>Vite + React</h1>
+
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <div className="counter">{`${count}回`}</div>
+        {!hasPermission && (
+          <button onClick={handlePermissionRequest}>Permission Request</button>
+        )}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="read-the-docs">端末を振ると鐘が鳴ります</p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
